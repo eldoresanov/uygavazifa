800890c4f57a6d9ce0373e06943a729700e4527d
Eldor [FE16] <118098131+eldoresanov@users.noreply.github.com>
2022-11-21 15:10:03 +0500

    Add files via upload

---
+++ uygavazifa-1/scripts.js
@@ -0,0 +1,6 @@
+let name = "eldor"
+let surname = "esanov"
+let age = 13
+let education = "Open Web Academy"
+let frontend = "FE16"
+console.log("My name is " + name + " " + surname + " im " + age + " years old " + " education: " + education + " cours: " + frontend )
